52cebc380d3dec98de14cecd04846bf92f180b0a
Sam Ho <twhltvproxy@gmail.com>
2020-09-03 10:51:32 +0800

    sth change

--- test06.php
+++ test06.php
@@ -1,6 +1,16 @@
+<h2>Break</h2>
+<?php
+    for($i = 0; $i < 10 ; $i++){
+        if ($i == 7) break; //當i是7時，會跳出這個迴圈，7、8、9都不會輸出
+        echo $i . "<br>"; //只會輸出0~6
+    }
+?>
+<hr>
+<h2>Continue</h2>
 <?php
     for($i = 0; $i < 10 ; $i++){
         if ($i%2 == 1) continue;
         echo $i . "<br>";        
     }
 ?>
+
